07f9f9d61c9ce5a36b2a6a12594b3e57e610db31
Bastien DUMONT <bastienxs@gmail.com>
2022-09-03 19:45:59 +0200

    seriallize user data

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/auth/user.entity';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -5,13 +6,14 @@ import { TaskStatus } from './task.-status.enum';
 import { Task } from './task.entity';
 
 export class TasksRepository extends Repository<Task> {
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = this.create({
       title: title,
       desription: description,
       status: TaskStatus.OPEN,
+      user,
     });
 
     await this.save(task);
